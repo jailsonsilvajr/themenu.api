c5eb64ab30159a76ae4561745c630be75fa2509c
Jailson Silva <jailson.silva@sorteonline.com.br>
2022-11-09 09:11:57 -0300

    Response with json, xml or csv.

--- TheMenu.API/TheMenu/Entensions/ServiceExtensions.cs
+++ TheMenu.API/TheMenu/Entensions/ServiceExtensions.cs
@@ -18,5 +18,8 @@ namespace TheMenu.Entensions
 
         public static void ConfigureServiceManager(this IServiceCollection services) =>
             services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder) =>
+            builder.AddMvcOptions(config => config.OutputFormatters.Add(new CsvOutputFormatter()));
     }
 }
